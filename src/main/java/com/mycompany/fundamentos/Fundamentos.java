b7c9d90c832576dd6027c653fac04edf68befd07
ALUMNO <ALUMNO@ALUMNO>
2026-02-24 15:57:26 -0500

    primer cargue

---
+++ src/main/java/com/mycompany/fundamentos/Fundamentos.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fundamentos;
+
+/**
+ *
+ * @author ALUMNO
+ */
+public class Fundamentos {
+
+    public static void main(String[] args) {
+        
+        int n1 = 8;
+        System.out.println("numero n1:" + n1);
+        int n2 = 7;
+        System.out.println("numero n2:" + n2);
+        int s = n1 + n2;
+        System.out.println("resultado:" + s);
+        
+    }
+}
